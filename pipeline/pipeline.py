09e8f972d8676cd1b51526dca180d3b53d090678
vyomagg <vyom135034@nitp.ac.in>
2020-10-21 23:56:01 +0530

    parameter format

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -83,8 +83,8 @@ def deploy_model_op(model):
     name='Regression Kubeflow Pipeline',
     description='An example pipeline that trains and logs a regression model.'
 )
-def regression_pipeline(train_samples: int=466, test_samples: int=50 , co_relation_threshold: float=.5 , fit_intercept: bool=True ,
-                        normalize: bool=False, n_jobs: int=2 , copy_X: bool=True, metrics: str="rsquare, mse, rmse, mae" ) :
+def regression_pipeline(train_samples, test_samples, co_relation_threshold, fit_intercept,
+                        normalize, n_jobs, copy_X, metrics ) :
 
     _extract_op = extract_data_op(train_samples, test_samples)
 
@@ -123,7 +123,8 @@ evaluate_params = params['evaluate']
 arguments = { 'train_samples' : extract_params['train_samples'], 'test_samples' : extract_params['test_samples'] ,
             'co_relation_threshold' : prepare_params['co_relation_threshold'] , 'fit_intercept' : train_params['fit_intercept'],
             'normalize' : train_params['normalize'], 'n_jobs' : train_params['n_jobs'] , 'copy_X' : train_params['copy_X'],
-            'metrics' : evaluate_params['metrics'] }
+            'metrics' : evaluate_params['metrics']}
+
 
 ## Auto Execution of pipeline
 client = kfp.Client(host='http://127.0.0.1:8081', namespace='kubeflow')
